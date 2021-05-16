11002bf53c1e8cf45f1708960d6d831aae7fbdb2
rm77249 <mehraraghav@johndeere.com>
2021-05-15 22:32:14 -0500

    find minimum number in list

---
+++ src/minimumNumberListComplexicityN.py
@@ -0,0 +1,20 @@
+from unittest.test.test_result import __init__
+
+
+def minimumNumberList(list1: list )-> int:
+    minimumNumber = list1[0]
+    for i in list1:
+        if i < minimumNumber:
+            minimumNumber = i
+
+    return minimumNumber
+
+if __name__ == '__main__':
+    minimum = minimumNumberList([5, 2, 7, 9, 3, 5, 76])
+    print(minimum)
+
+
+
+
+
+
